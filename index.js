45a0972f9ac6a4ab2d67af171d89955867866c59
rishowrav <rishowrav@gmail.com>
2024-04-29 00:44:39 +0600

    vercel install and deplaoy

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-// potteryDatabase TKVdyqBYMIFMXXZQ
-
+require("dotenv").config();
 const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
@@ -12,8 +11,7 @@ app.use(express.json());
 
 // MongoDB
 
-const uri =
-  "mongodb+srv://potteryDatabase:TKVdyqBYMIFMXXZQ@cluster0.sp25joa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.sp25joa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
